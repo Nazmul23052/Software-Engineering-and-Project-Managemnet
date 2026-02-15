7885232edfaf5134b594df1cd6daaf1adaafe80d
Nazmul23052 <it23052@mbstu.ac.bd>
2026-02-16 00:41:05 +0600

    Add DBConnection class for MySQL connection

---
+++ Quiz Game Using JavaFX and MySQL/DBConnection.java
@@ -0,0 +1,19 @@
+package com.quiz;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/quizdb",
+                    "root",
+                    "password");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
