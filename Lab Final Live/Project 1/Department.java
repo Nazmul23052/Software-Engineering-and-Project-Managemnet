833b1665c96d57e7128ef49921b9535aff2db598
Nazmul23052 <it23052@mbstu.ac.bd>
2026-02-16 13:22:15 +0600

    Add Department class with properties and methods

---
+++ Lab Final Live/Project 1/Department.java
@@ -0,0 +1,24 @@
+public class Department {
+    private int id;
+    private String name;
+    private int numberOfStudents;
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+}
